e785693c1d711f45567722218dd004530c0201c4
Kirisan Suthanthireswaran - CCS MD <mdcoop3@uoguelph.ca>
2023-09-25 16:17:18 -0400

    Updating p1, finished reading of stacks

--- Advent5/p1.c
+++ Advent5/p1.c
@@ -12,65 +12,113 @@ int main()
 {
 
     FILE * inFile;
-
-    inFile = fopen("test.txt", "r");
+    inFile = fopen("crates.txt", "r");
     char buffer[100];
     int numOfStacks = 0;
-    int check1 = 0;
+
+    int count = 0;
     int currentPosition = 1;
-    char crateArray[9][100];
+    char baseCrateArray[9][300];
+    char crateArray[9][300];
+    char deadBuffer[50];
+    char moveBuffer[100];
     int top[9] = {0};
-    while(!feof(inFile))
+
+    int amount = 0;
+    int from = 0;
+    int to = 0;
+
+    for(int i = 0; i < 8; i++)
     {
-        for(int i = 0; i < 9; i++)
-        {
-            fgets(crateArray[])
-        }
-
-
-
-
-
-
-        /*while(strcmp(fgets(buffer, 100, inFile), "\n") != 0)
-        {
-            currentPosition = 1;
-            printf("Loop (%s)\n", buffer);
-            //Run only once x
-            if (check1 == 0)
-            {
-                numOfStacks = strlen(buffer) / 4;
-
-                printf("NumOfStacks: %d\n", numOfStacks);
-                check1 = 1;
-            }
-
-            for(int i = numOfStacks-1; i >=0; i--)
-            {
-                printf("Current Crate: %c \n", buffer[currentPosition]);
-                if(buffer[currentPosition] != ' ')
-                {
-                    crateArray[i][top[i]] = buffer[currentPosition];
-                    top[i] ++;
-                }
-                currentPosition += 4;
-            }
-            //printf("%d\n", strlen(buffer) / 4);
-            sleep(1);
-
-            for(int i = 0; i < numOfStacks; i++)
-            {
-                for(int j = 0; j < top[i]+1; j++)
-                {
-                    printf("%c ", crateArray[i][j]);
-                }
-                printf("          Top [%d]\n", top[i]);
-            }
-
-
-
-        }*/
+        fgets(baseCrateArray[i], 300, inFile);
     }
 
+    for(int i = 8; i >= 0; i--)
+    {
+        //printf("Current: %s", baseCrateArray[i]);
+        count = 0;
+        currentPosition = 1;
+        
+        while(currentPosition < strlen(baseCrateArray[i]))
+        {
+            //printf("%c\n", baseCrateArray[i][currentPosition]);
+            if(baseCrateArray[i][currentPosition] != ' ')
+            {
+                crateArray[count][top[count]] = baseCrateArray[i][currentPosition];
+                top[count]++;
+            }
+            count++;
+            currentPosition += 4;
+        } 
+
+    }
+
+    for(int i = 0; i < 9; i++)
+    {
+        for(int j = 0; j < top[i]; j++)
+        {
+            printf("[%c] ", crateArray[i][j]);
+        }
+        printf("          Top [%d]\n", top[i]);
+    }
+
+    printf("\n\n\n");
+
+
+    //Skip two lines 
+    for(int i = 0; i < 2; i++)
+    {
+        fgets(deadBuffer, 100, inFile);
+    }
+
+    while(!feof(inFile))
+    {
+        fgets(moveBuffer, 100, inFile);
+
+        amount = (moveBuffer[5]) - '0';
+        from = (moveBuffer[12]) - '0'-1;
+        to = (moveBuffer[17]) - '0'-1;
+
+        printf("Amount: (%d)\nFrom: (%d)\nTo: (%d)\n", amount, from, to);
+
+        for(int i = 0 ; i < amount; i++)
+        {
+            if(top[from] >= 0)
+            {   
+                crateArray[to][top[to]] = crateArray[from][top[from] - 1];
+                crateArray[from][top[from] - 1] = ' ';
+
+                top[from]--;
+                top[to]++;
+            }
+        }  
+
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < top[i]; j++)
+            {
+                printf("[%c] ", crateArray[i][j]);
+            }
+            printf("          Top [%d]\n", top[i]);
+        } 
+        printf("\n");
+    }
+
+    printf("\n\n\n---------\n");
+    for(int i = 0; i < 9; i++)
+    {
+        for(int j = 0; j < top[i]; j++)
+        {
+            printf("[%c] ", crateArray[i][j]);
+        }
+        printf("          Top [%d]\n", top[i]);
+    }
+    
+
+    for(int i = 0; i < 9; i++)
+    {
+        printf("[%c] ", crateArray[i][top[i]-1]);
+    }
+    printf("\n");
     return 0;
 }
