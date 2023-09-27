431b0bb2291279eb2fad925d57c488a24e4067ad
Kirisan Suthanthireswaran - CCS MD <mdcoop3@uoguelph.ca>
2023-09-27 11:26:52 -0400

    Adding Advent6 and Finishing p1

---
+++ Advent6/p1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    FILE * inFile;
+    char packet[5000];
+    int check = 3;
+    int flag = 0;
+
+    inFile = fopen("packet.txt", "r");
+
+    while(!feof(inFile))
+    {
+        fgets(packet, 5000, inFile);
+        //printf("Packet: (%s)\n", packet);
+
+        while(check != strlen(packet))
+        {
+            for(int i = check; i > check-3; i--)
+            {
+                for(int j = i-1; j >= check-3; j--)
+                {
+                    if(flag == 0)
+                    {
+                        //printf("%c %c\n", packet[i], packet[j]);
+                        if(packet[i] == packet[j])
+                        {
+                            //printf("Match Found\n");
+                            flag = 1;
+                        }
+                    }
+                }
+            }
+            if(flag == 0)
+            {
+                printf("Check: %d\n", check+1);
+                check = strlen(packet)-1;
+            } 
+            flag = 0;
+            check++;
+        }
+    }
+}
